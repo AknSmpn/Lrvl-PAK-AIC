20f69957b70b784bad81552e498ccce416f95f38
AknSmpn <148300151+Cstaa@users.noreply.github.com>
2024-12-17 15:45:59 +0700

    add homepage

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\NewsController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('news',[NewsController::class,"index"]);
+Route::get('home',[HomeController::class,"index"]);
